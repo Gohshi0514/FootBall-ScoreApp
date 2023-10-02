fb9588f7e27ec600c256ae7d9dd964ee7d402589
Goshi <xxxxxxxgt@gmail.com>
2023-10-02 18:19:13 +0900

    save10/2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import useSWR from 'swr';
 import Link from 'next/link';
 
-const fetcher = (url: string) => fetch(url).then(res => res.json());
 
 export default function Home() {
-  const { data: standingsData, error: standingsError } = useSWR('/api/standings', fetcher);
-
-  if (standingsError) return <div>データの取得中にエラーが発生しました。</div>;
-  if (!standingsData) return <div>データを読み込んでいます...</div>;
 
   return (
     <>
--- src/pages/standings/index.tsx
+++ src/pages/standings/index.tsx
@@ -8,7 +8,7 @@ const fetcher = (url: string) => fetch(url).then(res => res.json());
 const Standings: React.FC = () => {
     const { data: standingsData, error: standingsError } = useSWR('/api/standings', fetcher);
 
-    if (standingsError) 
+    if (standingsError)
         return (
             <div className='flex items-center justify-center w-full h-screen'>
                 <div>データの取得中にエラーが発生しました。</div>
@@ -28,60 +28,62 @@ const Standings: React.FC = () => {
                     src={standingsData.competition.emblem}
                     alt={`${standingsData.competition.name} エンブレム`}
                     className='mx-auto'
-                    width={100}
-                    height={100}
+                    width={150}
+                    height={150}
                 />
             </h1>
-            <table className='flex flex-col items-center justify-center w-3/4 mx-auto my-4 border border-gray-300 rounded-md shadow-md'>
-                <thead className='w-full text-white bg-gray-800 rounded-t-md'>
-                    <tr className='flex flex-row justify-around w-full p-2'>
-                        <th className='w-1/12 flex-none flex-shrink-0'>順位</th>
-                        <th className='w-1/12 flex-none flex-shrink-0'>チーム</th>
-                        <th className='w-1/12 flex-none flex-shrink-0'>ポイント</th>
-                        <th className='w-1/12 flex-none flex-shrink-0'>試合数</th>
-                        <th className='w-1/12 flex-none flex-shrink-0'>勝利</th>
-                        <th className='w-1/12 flex-none flex-shrink-0'>引き分け</th>
-                        <th className='w-1/12 flex-none flex-shrink-0'>敗北</th>
-                        <th className='w-1/12 flex-none flex-shrink-0'>得点</th>
-                        <th className='w-1/12 flex-none flex-shrink-0'>失点</th>
-                        <th className='w-1/12 flex-none flex-shrink-0'>得失点差</th>
-                    </tr>
-                </thead>
-                <tbody className='flex flex-col w-full text-center divide-y divide-gray-300 bg-gray-100'>
-                    {standingsData.standings[0].table.map((team: any) => {
-                        return (
-                            <tr
-                                key={team.team.id}
-                                className='flex flex-row justify-around w-full p-2 hover:bg-gray-200'
-                            >
-                                <td className='w-1/12 flex-none flex-shrink-0 font-medium text-lg'>
-                                    {team.position}
-                                </td>
-                                <td className='flex flex-row items-center justify-center w-1/12 flex-none flex-shrink-0'>
-                                    <Image
-                                        src={team.team.crest}
-                                        alt={`${team.team.name} エンブレム`}
-                                        className='rounded-full'
-                                        width={30}
-                                        height={30}
-                                    />
-                                    <small className='block text-xs text-gray-500 rounded-full ml-2'>
-                                        {team.team.name}
-                                    </small>
-                                </td>
-                                <td className='w-1/12 flex-none flex-shrink-0 text-gray-600'>{team.points}</td>
-                                <td className='w-1/12 flex-none flex-shrink-0 text-gray-600'>{team.playedGames}</td>
-                                <td className='w-1/12 flex-none flex-shrink-0 text-gray-600'>{team.won}</td>
-                                <td className='w-1/12 flex-none flex-shrink-0 text-gray-600'>{team.draw}</td>
-                                <td className='w-1/12 flex-none flex-shrink-0 text-gray-600'>{team.lost}</td>
-                                <td className='w-1/12 flex-none flex-shrink-0 text-gray-600'>{team.goalsFor}</td>
-                                <td className='w-1/12 flex-none flex-shrink-0 text-gray-600'>{team.goalsAgainst}</td>
-                                <td className='w-1/12 flex-none flex-shrink-0 text-gray-600'>{team.goalDifference}</td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
+            <div className='flex flex-col items-center justify-center w-full px-5 md:px-0'>
+                <table className='flex flex-col items-center justify-center w-full md:w-3/4 mx-auto my-4 rounded-md shadow-md'>
+                    <thead className='w-full text-white bg-gray-800 rounded-t-md'>
+                        <tr className='flex flex-row justify-around w-full p-2'>
+                            <th className='md:w-1/12 flex-none flex-shrink-0'>順位</th>
+                            <th className='md:w-1/12 flex-none flex-shrink-0'>チーム</th>
+                            <th className='md:w-1/12 flex-none flex-shrink-0'>勝ち点</th>
+                            <th className='w-1/12 flex-none flex-shrink-0 hidden md:table-cell'>試合</th>
+                            <th className='w-1/12 flex-none flex-shrink-0 hidden md:table-cell'>勝ち</th>
+                            <th className='w-1/12 flex-none flex-shrink-0 hidden md:table-cell'>引き分け</th>
+                            <th className='w-1/12 flex-none flex-shrink-0 hidden md:table-cell'>負け</th>
+                            <th className='w-1/12 flex-none flex-shrink-0 hidden md:table-cell'>得点</th>
+                            <th className='w-1/12 flex-none flex-shrink-0 hidden md:table-cell'>失点</th>
+                            <th className='w-1/12 flex-none flex-shrink-0 hidden md:table-cell'>得失点差</th>
+                        </tr>
+                    </thead>
+                    <tbody className='flex flex-col w-full text-center divide-y divide-gray-300 bg-gray-100'>
+                        {standingsData.standings[0].table.map((team: any) => {
+                            return (
+                                <tr
+                                    key={team.team.id}
+                                    className='flex flex-row justify-around w-full p-2 hover:bg-gray-200'
+                                >
+                                    <td className='w-1/12 flex-none flex-shrink-0 font-medium text-sm md:text-lg'>
+                                        {team.position}
+                                    </td>
+                                    <td className='flex flex-row items-center justify-center w-1/12 flex-none flex-shrink-0'>
+                                        <Image
+                                            src={team.team.crest}
+                                            alt={`${team.team.name} エンブレム`}
+                                            className='rounded-full'
+                                            width={30}
+                                            height={30}
+                                        />
+                                        <small className='text-xs text-gray-500 rounded-full ml-2'>
+                                            {team.team.name}
+                                        </small>
+                                    </td>
+                                    <td className='w-1/12 flex-none flex-shrink-0 md:text-gray-600'>{team.points}</td>
+                                    <td className='w-1/12 flex-none flex-shrink-0 md:text-gray-600 hidden md:table-cell'>{team.playedGames}</td>
+                                    <td className='w-1/12 flex-none flex-shrink-0 md:text-gray-600 hidden md:table-cell'>{team.won}</td>
+                                    <td className='w-1/12 flex-none flex-shrink-0 md:text-gray-600 hidden md:table-cell'>{team.draw}</td>
+                                    <td className='w-1/12 flex-none flex-shrink-0 md:text-gray-600 hidden md:table-cell'>{team.lost}</td>
+                                    <td className='w-1/12 flex-none flex-shrink-0 md:text-gray-600 hidden md:table-cell'>{team.goalsFor}</td>
+                                    <td className='w-1/12 flex-none flex-shrink-0 md:text-gray-600 hidden md:table-cell'>{team.goalsAgainst}</td>
+                                    <td className='w-1/12 flex-none flex-shrink-0 md:text-gray-600 hidden md:table-cell'>{team.goalDifference}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 }
